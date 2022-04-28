44296dfc0b78c8cd1527696183f94b7f938a3507
istrelkina <istrelkina@gmail.com>
2022-04-28 10:14:05 +1000

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+  function sortFn(a, b) {
+    return a.toLowerCase().charCodeAt(0) - b.toLowerCase().charCodeAt(0)
+  }
+  let result = '';
+  let resultStr;
+  console.log(members);
+  if (members == '' || members === null || members === undefined) return false;
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) === 'string') {
+      let newElement = members[i].replace(/\s+/g, '');
+      if (newElement.charCodeAt(0) >= 65 && newElement.charCodeAt(0) <= 90 || newElement.charCodeAt(0) >= 97 && newElement.charCodeAt(0) <= 122) {
+
+        result += newElement[0];
+      };
+    }
+  };
+
+
+  if (result === '' || !(typeof (result) === 'string')) {
+    return false;
+  } else {
+    return result.split('').sort(sortFn).join('').toUpperCase();
+  }
+};
+
 
 module.exports = {
   createDreamTeam
