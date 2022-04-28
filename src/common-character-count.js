3f8b267c1d280476009795fb45cf1722bf9bd82a
istrelkina <istrelkina@gmail.com>
2022-04-28 13:42:18 +1000

    feat: add delete-digit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let symbol = '';
+  let symbCount1 = 0;
+  let symbCount2 = 0;
+  let count = 0;
+
+  console.log(s1, '   ', s2);
+
+  for (let i = 97; i <= 122; i++) {
+    symbol = String.fromCharCode(i);
+    symbCount1 = s1.split(symbol).length - 1;
+    symbCount2 = s2.split(symbol).length - 1;
+
+    if (symbCount1 <= symbCount2) {
+      count += symbCount1;
+    } else {
+      count += symbCount2;
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let str = n + '';
+  let min = str[0];
+  let index = 0;
+  let resultStr = '';
+  console.log('n = ', n);
+  for (let i = 1; i < str.length; i++) {
+    console.log(i);
+    console.log(str[i]);
+    if (str[i] > min) {
+      // console.log(str.substring(0, i - 1));
+      for (let j = 0; j < str.length; j++)
+        if (!(j === index)) {
+          resultStr += str[j];
+        }
+      console.log('result = ', resultStr);
+      return parseInt(resultStr, 10);
+    } else {
+      min = str[i];
+      index = i;
+    }
+  }
+  for (let j = 0; j < str.length; j++)
+    if (!(j === index)) {
+      resultStr += str[j];
+    }
+  console.log('result = ', resultStr);
+  return parseInt(resultStr, 10);
 }
 
 module.exports = {
